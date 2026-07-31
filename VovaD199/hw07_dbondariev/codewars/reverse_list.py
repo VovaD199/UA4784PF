a992136e23fb61e13a59ccb2f7c9830059219003
Dmytro Bondariev <Dmytro.Bondary@bd.com>
2026-07-31 12:50:26 +0300

    codewars practice

---
+++ VovaD199/hw07_dbondariev/codewars/reverse_list.py
@@ -0,0 +1,5 @@
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+
+reverse_list = lambda lst: lst[::-1]
+
+print(reverse_list(my_list))
---
+++ VovaD199/hw07_dbondariev/codewars/string_reverse.py
@@ -0,0 +1,3 @@
+reverse = lambda st: " ".join(st.split()[::-1])
+
+print(reverse("Hello World! This is a test."))
