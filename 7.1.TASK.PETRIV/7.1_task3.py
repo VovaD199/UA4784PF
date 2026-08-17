122a1d4bd5066ac906d658d074c3fcf910a97711
wozeuleaplug <144951090+wozeuleaplug@users.noreply.github.com>
2026-08-17 22:37:08 +0300

    Task7.3 letter counting using dict.get() method. Final version

--- 7.1.TASK.PETRIV/7.1_task3.py
+++ 7.1.TASK.PETRIV/7.1_task3.py
@@ -1,11 +1,12 @@
 word = input("Enter a word: ")
+
 def count_letters(word):
     letter_count = {}
+
     for letter in word:
-        if letter in letter_count:
-            letter_count[letter] += 1
-        else:
-            letter_count[letter] = 1
+        letter_count[letter] = letter_count.get(letter, 0) + 1
+
     return letter_count
+
 
 print(count_letters(word))
