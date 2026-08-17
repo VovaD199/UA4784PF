f837cd5d4e2896a06518e1abce177deb12896d50
wozeuleaplug <144951090+wozeuleaplug@users.noreply.github.com>
2026-08-17 22:21:29 +0300

    Updated Task7.2 as function and tried different method to count letters

--- 7.1.TASK.PETRIV/7.1_task2.py
+++ 7.1.TASK.PETRIV/7.1_task2.py
@@ -1,9 +1,11 @@
-user_enter = str(input("Enter a word: "))
-letter_count = {}
-for letter in user_enter:
-    if letter in letter_count:
-        letter_count[letter] += 1
-    else:
-        letter_count[letter] = 1
+def count_letters(word):
+    letter_count = {}
 
-print(letter_count)
+    for letter in word:
+        letter_count[letter] = letter_count.get(letter, 0) + 1
+
+    return letter_count
+
+
+word = input("Enter a word: ")
+print(count_letters(word))
