d1cbc2ced55972bee7847b349337062945121a64
oleksiiantypov-ui <oleksii.antypov@gmail.com>
2026-08-21 13:35:49 +0300

    Update HW10.3_Task3.py

--- oleksiiantypov-ui/HW10.3_Task3.py
+++ oleksiiantypov-ui/HW10.3_Task3.py
@@ -1,26 +1,3 @@
-
-
-
-# class Human:
-#     def __init__(self, name):
-#         self.name = name
-
-#     def message(self):
-#         print(f'Welcome {self.name}')
-
-#     @classmethod
-#     def class_info(cls):
-#         return 'it is a species of Homosapiens'
-
-#     @staticmethod
-#     def static_method():
-#         return f'It is static method'
-
-# one = Human('Oleksii')
-# one.message()
-# print(Human.class_info())
-# print(one.static_method())
-
 class Employee:
     employeesCount: int = 0 
 
